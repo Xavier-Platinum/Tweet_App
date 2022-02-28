be39153d6678ed05df14dd769b87f5cbe8b4e8a5
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-01 00:33:04 +0100

    Creating the new Post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,9 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
   end
+  
+  def new
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  
   get "posts/:id" => "posts#show"
   
   get "/" => "home#top"
