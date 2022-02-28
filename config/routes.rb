057fffccb688e48980a9e8d58e4130f6796476c1
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-28 07:19:04 +0100

    Understanding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "home/top" => "home#top"
 end
