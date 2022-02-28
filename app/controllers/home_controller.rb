d33fa9409988b24bc7ba26c2b49e9337306772d9
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-28 07:36:33 +0100

    The about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def top
+
+  end
+
+  # about
+  def about
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # Rewrite "home/top"
   get "top" => "home#top"
+
+  # about page
+  get "about" => "home#about"
 end
