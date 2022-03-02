708b86d006304e3956ee5c643fc7ad11eb7d3008
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-02 13:18:04 +0100

    deleting post(3)

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
   
-  
   get "/" => "home#top"
   get "about" => "home#about"
 end
