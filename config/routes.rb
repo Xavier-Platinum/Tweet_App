b0bec2e9706708d8a6f00fa51cb49ff25b4834c0
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-02 02:59:50 +0100

    getting the form input data in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/update" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
   
   
   get "/" => "home#top"
