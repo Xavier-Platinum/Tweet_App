7374766c0b1da982eb5f3747a093380eb52ce9e4
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-05 23:40:26 +0100

    creating the table and the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_101759) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_05_223855) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
