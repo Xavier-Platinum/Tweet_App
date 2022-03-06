b20bf379443e2808129b31c57547acd23e21c1cc
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-06 22:02:13 +0100

    Adding validation to create roiute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,17 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
   
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice] = "You have signed up successfully"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
+  
 end
