9335ffcb0465de0fc30cbe1d835b8ac26f748a45
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-06 05:29:08 +0100

    creating the users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-    # Validate that the name column is not empty
     validates :name, {presence: true}
-    
-    # Validate that the email column is unique and not empty
     validates :email, {presence: true, uniqueness: true}
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index" => "users#index"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
-
+  
   get "/" => "home#top"
   get "about" => "home#about"
 end
