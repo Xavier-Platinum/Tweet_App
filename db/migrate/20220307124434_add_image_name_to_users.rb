02c12269af80a8da2386ab50f091a3647fcf9908
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-07 14:11:46 +0100

    editing the migrations file

--- db/migrate/20220307124434_add_image_name_to_users.rb
+++ db/migrate/20220307124434_add_image_name_to_users.rb
@@ -1,4 +1,5 @@
-class AddImageNameToUsers < ActiveRecord::Migration[7.0]
+class AddImageNameToUsers < ActiveRecord::Migration[5.0]
   def change
+    add_column :users, :image_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_05_223855) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_07_124434) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_05_223855) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
