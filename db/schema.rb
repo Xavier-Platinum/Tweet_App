d67347b15cbe68df6f8697dc5f917cb630a91001
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-07 21:57:12 +0100

    Adding the password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_07_124434) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_07_204232) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_07_124434) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
