504ef25872128d6f27cb7b79f0fd90a24b5da906
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-08 22:58:36 +0100

    Authenticating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, {only: {:index, :show, :edit, :update}}
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   
   def index
     @users = User.all
