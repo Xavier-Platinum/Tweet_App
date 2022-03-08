79980a243dfedcdfd716fa0ce857be7e31a71909
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-08 22:51:47 +0100

    Authenticating user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
     before_action :set_current_user
     
     def set_current_user
-        @current_user = User.find_by(id: session[:user_id])
+      @current_user = User.find_by(id: session[:user_id])
     end
-
-end
+    
+    def authenticate_user
+      if @current_user == nil
+        flash[:notice] = "You must be logged in"
+        redirect_to("/login")
+    end
+  end
+  
