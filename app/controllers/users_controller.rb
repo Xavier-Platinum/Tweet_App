5f98f6b0844e103a432f38f074c2c8d22e323904
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-08 23:26:18 +0100

    Authenticating the edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
   before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   
   def index
     @users = User.all
@@ -74,6 +75,13 @@ class UsersController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "You have logged out successfully"
     redirect_to("/login")
+  end
+  
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "Unauthorized access"
+      redirect_to("/posts/index")
+    end
   end
   
 end
