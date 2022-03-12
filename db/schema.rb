755cba36e64e28905e3b38e469ab3180a04cb56c
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-12 12:47:24 +0100

    The Like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_10_221952) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_110025) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
