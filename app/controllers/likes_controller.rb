4f5abdac928c255100f1e6b82ba08896d6845d1e
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-12 13:14:55 +0100

    The likes button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,10 @@
 class LikesController < ApplicationController
     before_action :authenticate_user
-    
+  
     def create
+        @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
+        @like.save
+        redirect_to("/posts/#{params[:post_id]}")
+      
     end
-    
 end
