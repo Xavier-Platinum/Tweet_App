6a5041b0f14a384244676dc837f9c29b25b4b949
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-12 08:30:29 +0100

    Using instance method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find_by(id: params[:id])
-    @user = User.find_by(id: @post.user_id)
+    @user = @post.user
   end
   
   def new
