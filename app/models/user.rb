b2f65bf8eeac7bfe5c5e5032c5130b4988a00394
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-12 08:59:08 +0100

    Getting the user's Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
     validates :password, {presence: true}
+    
+    def posts
+        return Post.where(user_id: self.id)
+    end
+    
 end
