f99fafa0adf391468acb9c400d486206dff2d40f
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-03-13 06:56:50 +0100

    Adding password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_110025) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_13_055343) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_110025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
